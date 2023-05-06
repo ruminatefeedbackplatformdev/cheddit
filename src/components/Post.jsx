8e8ff371e01e728a785692c6fcf2ba8a81b917c5
Sean Eric Thomas <set@seanericthomas.com>
2023-05-05 17:42:09 -0700

    Handle newline characters in post content string prop

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,13 +9,13 @@ export default function Post({
   subject,
   time,
 }) {
+  const splitContent = content.split('\\n');
+  console.log(splitContent);
   return (
     <article className="post" id={number}>
       <span className="post-info">
         {subject ? <span aria-label="subject">{subject}</span> : null}
-        <span aria-label="author">
-          {author !== undefined ? author : 'Anonymous'}
-        </span>
+        <span aria-label="author">{author || 'Anonymous'}</span>
         <span aria-label="timestamp">{new Date(time).toLocaleString()}</span>
         <span aria-label="post number">{`#${number}`}</span>
         {replies.length === 0
@@ -28,7 +28,19 @@ export default function Post({
       </span>
       <span>
         {image ? <img src={image} alt="" /> : null}
-        <span aria-label="post content">{content}</span>
+        <span aria-label="post content">
+          {content.split('\\n').map((line, index) => (
+            // post content comes from a <textarea> which can contain
+            // newline "\n" characters as part of the string stored in
+            // the cloud firestore database.
+
+            // seem safe to use the index as part of the key here, since
+            // we're also using the unique post number.
+
+            // eslint-disable-next-line
+            <span key={`#${number}-line${index}`}>{line}</span>
+          ))}
+        </span>
       </span>
     </article>
   );
