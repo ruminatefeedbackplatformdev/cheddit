0d9aa7c2b20219d33acecb695b6b83bc7b030224
Sean Eric Thomas <set@seanericthomas.com>
2023-05-09 15:11:24 -0700

    Show thread post counts

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -31,11 +31,29 @@ async function loadThreads(id) {
   return firstPosts;
 }
 
+async function getPostCounts(id) {
+  const { posts, threads } = await loadBoard(id);
+  const postKeys = Object.keys(posts);
+  const postCounts = {};
+  threads.forEach((thread) => {
+    postCounts[thread] = 0;
+  });
+
+  postKeys.forEach((key) => {
+    const thisPost = posts[key];
+    postCounts[thisPost.thread] += 1;
+  });
+
+  return postCounts;
+}
+
 export default function Board({ id, name }) {
   const [threads, setThreads] = useState([]);
+  const [postCounts, setPostCounts] = useState({});
 
   const readDatabase = async () => {
     setThreads(await loadThreads(id));
+    setPostCounts(await getPostCounts(id));
   };
 
   useEffect(() => {
@@ -48,17 +66,19 @@ export default function Board({ id, name }) {
         <h1>{`/${id}/ - ${name}`}</h1>
         <NewThread board={id} readDatabase={readDatabase} />
         {threads.map((thread) => (
-          <Post
-            author={thread.author}
-            content={thread.content}
-            image={thread.image}
-            key={thread.number}
-            number={thread.number}
-            replies={thread.replies}
-            subject={thread.subject}
-            thread={thread.number}
-            time={thread.time}
-          />
+          <div key={`board${id}-post#${thread.number}`}>
+            <Post
+              author={thread.author}
+              content={thread.content}
+              image={thread.image}
+              number={thread.number}
+              replies={thread.replies}
+              subject={thread.subject}
+              thread={thread.number}
+              time={thread.time}
+            />
+            <span>{`${postCounts[thread.number] - 1} replies.`}</span>
+          </div>
         ))}
       </main>
     );
