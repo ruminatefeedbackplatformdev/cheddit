8071fb9c68c9a40d28c3cbd7c5a0a733fc81d40c
Sean Eric Thomas <set@seanericthomas.com>
2023-05-11 12:10:18 -0700

    Handle routing in RouteSwitch component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { collection, getDocs } from 'firebase/firestore';
-import database from '../util/firestore';
 import Header from './Header';
-import Home from './Home';
-import Dashboard from './Dashboard';
-import Board from './Board';
-import Thread from './Thread';
+import RouteSwitch from './RouteSwitch';
 import Footer from './Footer';
 
 export default function App() {
@@ -15,19 +9,6 @@ export default function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const readDatabase = async () => {
-      const querySnapshot = await getDocs(collection(database, 'boards'));
-      const allBoards = [];
-      querySnapshot.forEach((doc) => {
-        const thisBoard = {};
-        thisBoard.id = doc.id;
-        thisBoard.name = doc.data().name;
-        thisBoard.threads = doc.data().threads;
-        allBoards.push(thisBoard);
-      });
-      setBoards(allBoards);
-    };
-    readDatabase();
     const auth = getAuth();
     onAuthStateChanged(auth, (authUser) => {
       setUser(authUser);
@@ -37,30 +18,7 @@ export default function App() {
   return (
     <div className="container">
       <Header user={user} />
-      <Routes>
-        <Route
-          path="/"
-          element={<Home boards={boards} setBoards={setBoards} />}
-        />
-        <Route
-          path="/dash"
-          element={<Dashboard user={user} />}
-        />
-        {boards.map((board) => (
-          <Route
-            key={`route-${board.id}`}
-            path={`${board.id}`}
-            element={<Board id={board.id} name={board.name} />}
-          />
-        ))}
-        {boards.map((board) => board.threads.map((thread) => (
-          <Route
-            key={`route-${board.id}_thread-${thread}`}
-            path={`${board.id}_t${thread}`}
-            element={<Thread board={board.id} name={board.name} op={thread} />}
-          />
-        )))}
-      </Routes>
+      <RouteSwitch boards={boards} setBoards={setBoards} user={user} />
       <Footer />
     </div>
   );
---
+++ src/components/RouteSwitch.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import database from '../util/firestore';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import Board from './Board';
+import Thread from './Thread';
+
+export default function RouteSwitch({ boards, setBoards, user }) {
+  useEffect(() => {
+    const q = query(collection(database, 'boards'));
+    onSnapshot(q, (querySnapshot) => {
+      const allBoards = [];
+      querySnapshot.forEach((doc) => {
+        const thisBoard = {};
+        thisBoard.id = doc.id;
+        thisBoard.name = doc.data().name;
+        thisBoard.threads = doc.data().threads;
+        allBoards.push(thisBoard);
+      });
+      setBoards(allBoards);
+    });
+  }, []);
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<Home boards={boards} setBoards={setBoards} />}
+      />
+      <Route path="/dash" element={<Dashboard user={user} />} />
+      {boards.map((board) => (
+        <Route
+          key={`route-${board.id}`}
+          path={`${board.id}`}
+          element={<Board id={board.id} name={board.name} />}
+        />
+      ))}
+      {boards.map((board) => board.threads.map((thread) => (
+        <Route
+          key={`route-${board.id}_thread-${thread}`}
+          path={`${board.id}_t${thread}`}
+          element={<Thread board={board.id} name={board.name} op={thread} />}
+        />
+      )))}
+    </Routes>
+  );
+}
