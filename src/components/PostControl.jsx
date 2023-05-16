d75abf06b94ec862239d0c10e016c6f9c785ec72
Sean Eric Thomas <set@seanericthomas.com>
2023-05-16 13:04:26 -0700

    Confirm with user before deleting threads / posts

--- src/components/PostControl.jsx
+++ src/components/PostControl.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   arrayRemove, deleteField, doc, updateDoc,
 } from 'firebase/firestore';
@@ -7,6 +7,16 @@ import database from '../util/firestore';
 
 export default function PostControl({ board, number, thread }) {
   const navigate = useNavigate();
+
+  const [confirming, setConfirming] = useState(false);
+
+  const cancelDelete = () => {
+    setConfirming(false);
+  };
+
+  const promptConfirm = async () => {
+    setConfirming(true);
+  };
 
   const deletePost = async () => {
     const boardRef = doc(database, 'boards', board);
@@ -23,19 +33,61 @@ export default function PostControl({ board, number, thread }) {
     await updateDoc(boardRef, {
       threads: arrayRemove(thread),
     });
+    setConfirming(false);
     navigate(`/${board}`);
   };
 
   if (number === thread) {
     return (
-      <button className="delete-post" onClick={deleteThread} type="button">
-        DELETE THREAD
-      </button>
+      <span className="post-control">
+        <button
+          className={confirming ? 'delete-post hidden' : 'delete-post visible'}
+          onClick={promptConfirm}
+          type="button"
+        >
+          DELETE THREAD
+        </button>
+        <span className={confirming ? 'confirm visible' : 'confirm hidden'}>
+          <button
+            className="delete-confirm"
+            onClick={deleteThread}
+            type="button"
+          >
+            CONFIRM DELETE
+          </button>
+          <button
+            className="delete-confirm"
+            onClick={cancelDelete}
+            type="button"
+          >
+            CANCEL
+          </button>
+          <span>
+            Are you sure? Threads and their posts cannot be recovered once
+            deleted!
+          </span>
+        </span>
+      </span>
     );
   }
   return (
-    <button className="delete-post" onClick={deletePost} type="button">
-      DELETE POST
-    </button>
+    <span className="post-control">
+      <button
+        className={confirming ? 'delete-post hidden' : 'delete-post visible'}
+        onClick={promptConfirm}
+        type="button"
+      >
+        DELETE POST
+      </button>
+      <span className={confirming ? 'confirm visible' : 'confirm hidden'}>
+        <button className="delete-confirm" onClick={deletePost} type="button">
+          CONFIRM DELETE
+        </button>
+        <button className="delete-confirm" onClick={cancelDelete} type="button">
+          CANCEL
+        </button>
+        <span>Are you sure? Posts cannot be recovered once deleted!</span>
+      </span>
+    </span>
   );
 }
