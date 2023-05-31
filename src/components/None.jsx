0128cfacd07820baad051cdb2ae92e4708bd93d3
Sean Eric Thomas <set@seanericthomas.com>
2023-05-31 13:34:38 -0700

    Add "loading" gif to None component

--- src/components/None.jsx
+++ src/components/None.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import hourglass from '../images/loading.gif';
 
 export default function None() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
+
   return (
-    <div>
-      Nothing to see here...perhaps you&apos;ve visited a bad link or entered an
-      invalid URL? Try heading back
-      {' '}
-      <Link to="/">home</Link>
-      .
+    <div className="invalid-route">
+      {loading ? (
+        <span className="loading">
+          Loading...
+          <img src={hourglass} alt="" />
+        </span>
+      ) : (
+        <span>
+          Nothing to see here...perhaps you&apos;ve visited a bad link or
+          entered an invalid URL? Try heading back
+          {' '}
+          <Link to="/">home</Link>
+          .
+        </span>
+      )}
     </div>
   );
 }
