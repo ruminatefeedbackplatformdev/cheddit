b24fb763bf5cefe0defe4087cc95a4c3641656f8
Sean Eric Thomas <set@seanericthomas.com>
2023-06-05 14:42:07 -0700

    Style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer>
-      By Sean Eric Thomas
+      <span>
+        <Link to="/about">About Cheddit</Link>
+      </span>
+      <span>
+        Designed by
+        {' '}
+        <a
+          target="_blank"
+          href="http://www.seanericthomas.com"
+          rel="noreferrer"
+        >
+          Sean Eric Thomas
+        </a>
+      </span>
     </footer>
   );
 }
